976abac8d424950bd75c1e72c5a8ef9d749d9cc7
Nils Druyen <n.druyen@appcom-interactive.de>
2019-01-26 12:15:51 +0100

    Modify main activity

--- app/src/main/java/eu/appcom/rubhackathon/activities/main/MainActivity.kt
+++ app/src/main/java/eu/appcom/rubhackathon/activities/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package eu.appcom.rubhackathon.activities.main
 
+import android.os.Bundle
 import eu.appcom.rubhackathon.R
+import eu.appcom.rubhackathon.activities.game.GameActivity
 import eu.appcom.rubhackathon.base.BaseActivity
 import eu.appcom.rubhackathon.base.BaseContract
+import eu.appcom.rubhackathon.extensions.launchActivity
+import eu.appcom.rubhackathon.extensions.onClick
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), MainContract.MainView {
@@ -13,5 +18,13 @@ class MainActivity : BaseActivity(), MainContract.MainView {
   override fun providePresenter(): BaseContract.BasePresenter = presenter
 
   override fun provideLayoutResId(): Int = R.layout.activity_main
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    main_game_button.onClick {
+      launchActivity<GameActivity> {}
+    }
+  }
 
 }
