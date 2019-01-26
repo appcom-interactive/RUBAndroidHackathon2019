f90d80d1479736d53be43dc60843ffd880ec8673
Tobias Küper <t.kueper@appcom-interactive.de>
2019-01-26 10:58:11 +0100

    Add controllers to di

--- app/src/main/java/eu/appcom/rubhackathon/di/ActivityModule.kt
+++ app/src/main/java/eu/appcom/rubhackathon/di/ActivityModule.kt
@@ -5,6 +5,10 @@ import dagger.Binds
 import dagger.Module
 import eu.appcom.rubhackathon.annotations.PerActivity
 import eu.appcom.rubhackathon.base.BaseActivity
+import eu.appcom.rubhackathon.controllers.CommandController
+import eu.appcom.rubhackathon.controllers.CommandControllerImpl
+import eu.appcom.rubhackathon.controllers.SpeechController
+import eu.appcom.rubhackathon.controllers.SpeechControllerImpl
 import eu.appcom.rubhackathon.utils.Constants.ACTIVITY
 import javax.inject.Named
 
@@ -24,4 +28,11 @@ abstract class ControllerModule {
   @Named(ACTIVITY)
   internal abstract fun bindActivityContext(activity: BaseActivity): Context
 
+  @Binds
+  @PerActivity
+  abstract fun bindCommandController(commandControllerImpl: CommandControllerImpl): CommandController
+
+  @Binds
+  @PerActivity
+  abstract fun bindSpeechController(speechControllerImpl: SpeechControllerImpl): SpeechController
 }
